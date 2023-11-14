440bf20fac45623de881c72a78ee0dd4c6232b4d
Romário Lima <devromariolima@gmail.com>
2023-11-14 17:41:25 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+async function  ObterCep () {
+
+
+ const cep = document.getElementById("cep");
+
+ let search = cep.value.replace("-","")
+
+ const response = await fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+.then((response) => response.json())
+
+    // console.log(response);
+    ExibirDados(response);
+    
+
+  
+
+};
+
+const options = {
+  method: 'Get',
+  mode: 'cors',
+  Cache: 'default',
+}
+
+
+
+function ExibirDados (response){
+  document.getElementById('logradouro').innerHTML = response.logradouro;
+  document.getElementById('complemento').innerHTML = response.complemento;
+  document.getElementById('bairro').innerHTML = response.bairro;
+  document.getElementById('localidade').innerHTML = response.localidade;
+
+
+
+}
