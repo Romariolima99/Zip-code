4673d2b1c408375175e3fa26b141a74570952f70
Romário Lima <devromariolima@gmail.com>
2023-11-16 16:11:40 -0300

    Ajustes no fetch

--- main.js
+++ main.js
@@ -2,14 +2,25 @@
 
 async function  ObterCep () {
  const cep = document.getElementById("cep");
- let search = cep.value.replace("-","")
- const response = await fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-.then((response) => response.json())
 
-    console.log(response);
-    ExibirDados(response);
-    
-};
+ let search = cep.value.replace("-","")
+
+ if (search.length > 8) {
+  alert("informe uma quntidade de numeros validos")
+  return;
+ }
+
+ const response = await fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+.then((response) => response.json()) 
+.then((data) => { 
+  ExibirDados(data);
+})
+.catch((error) => {
+  if (error === true) {
+    alert("cep invalido")
+   }
+});
+}
 
 const options = {
   method: 'Get',
@@ -18,7 +29,7 @@ const options = {
 }
 
 function ExibirDados (response){
-  document.getElementById('logradouro').innerHTML = "logradouro: " + response.logradouro;
+  document.getElementById('logradouro').innerHTML = "Logradouro: " + response.logradouro;
   document.getElementById('bairro').innerHTML = "Bairro: " + response.bairro;
   document.getElementById('localidade').innerHTML = "Localidade: " + response.localidade;
   document.getElementById('ibge').innerHTML = "Numero ibge: " + response.ibge;
@@ -26,3 +37,4 @@ function ExibirDados (response){
 
 
 }
+
